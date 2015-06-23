5f2ed304f5fc2f0a4022856d451e0ce73221eb32
gagomet <kir_one@list.ru>
2015-06-23 18:11:13 +0300

    add fxml probe

--- src/main/java/coolor/fx/Starter.java
+++ src/main/java/coolor/fx/Starter.java
@@ -1,20 +1,50 @@
 package coolor.fx;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
+
 public class Starter extends Application {
+    private Stage primaryStage;
+    private Pane rootLayout;
 
     @Override
-    public void start(Stage stage) throws Exception {
-        stage.setTitle("Coolor");
-        Button button = new Button("Button");
-        StackPane stackPane = new StackPane();
-        stackPane.getChildren().add(button);
-        stage.setScene(new Scene(stackPane, 640, 480));
-        stage.show();
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+        initRootLayout();
+
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            loader.setLocation(classloader.getResource("mainPane.fxml"));
+            rootLayout = (Pane)loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
