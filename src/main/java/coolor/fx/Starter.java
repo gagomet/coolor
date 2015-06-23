743b78b67a8cd38487b445cc1ffc3fd2eeb57f26
gagomet <kir_one@list.ru>
2015-06-23 17:18:48 +0300

    add fxml probe

--- src/main/java/coolor/fx/Starter.java
+++ src/main/java/coolor/fx/Starter.java
@@ -1,12 +1,20 @@
 package coolor.fx;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Starter extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-
+        stage.setTitle("Coolor");
+        Button button = new Button("Button");
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(button);
+        stage.setScene(new Scene(stackPane, 640, 480));
+        stage.show();
     }
 }
