9a297c06748fb4bee58fdf5f3f9cb7e709253914
gagomet <kir_one@list.ru>
2015-09-01 14:41:21 +0300

    export to xls in progress (cherry picked from commit 0644e08)

---
+++ src/main/java/coolor/fx/Feature.java
@@ -0,0 +1,13 @@
+package coolor.fx;
+
+public class Feature {
+    private String feature;
+
+    public String getFeature() {
+        return feature;
+    }
+
+    public void setFeature(String feature) {
+        this.feature = feature;
+    }
+}
